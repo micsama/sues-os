0e936aae8ff723157ba9f416bc093b91dc40ee78
damyjy <zsqw123@vip.qq.com>
2021-11-28 23:58:58 +0800

    add: 充值电费

--- electricFeeAdd.py
+++ electricFeeAdd.py
@@ -1,12 +1,18 @@
+# 请前往 https://epay.sues.edu.cn/ 设置支付密码, 否则无法完成校园卡充值
+#
+# 这玩意需要传入四个参数, 充值未必成功, 因为OCR存在失败概率
+# 学号 校园卡密码 充值金额 支付密码
+#
 # pip install easyocr
 
-from math import trunc
 import people
 import random
+from retrying import retry
 from requests.sessions import Session
+from bs4 import BeautifulSoup
 import easyocr
-from PIL import Image
-from util.log import log
+from util.log import debug, log
+from util.utils import getStartArgs
 
 __base = 'https://epay.sues.edu.cn'
 # 验证码
@@ -14,7 +20,30 @@ __code = __base+'/epay/codeimage?'+str(random.randint(1001, 9998))
 # 登录
 __login = __base+'/epay/j_spring_security_check'
 # 查
-__bill = '/epay/electric/queryelectricbill'
+__bill = __base+'/epay/electric/queryelectricbill'
+
+__prePay = __base+'/epay/electric/load4paidelectricbill'
+__realPay = __base+'/epay/electric/payconfirm'
+# https://epay.sues.edu.cn/epay/electric/load4paidelectricbill
+# elcsysid: 1
+# elcarea: 1
+# elcbuis: 10
+# roomNo: 2015
+# dumpEnergy: 161.21
+# paidMoney: 0.01
+# route:
+
+
+# <input id="billno" name="billno" type="hidden" value="2c9488a07ce37b3c017"/>
+# <input id="refno" name="refno" type="hidden" value="202111282222"/>
+
+# https://epay.sues.edu.cn/epay/electric/payconfirm
+
+# billno: 2c9488a07ce37b3c017
+# refno: 20211128222
+# status: 0
+# banktype:
+# paypwd: input!
 
 
 __elecItem = {
@@ -36,16 +65,70 @@ def __getCode(sess: Session) -> str:
     return code
 
 
-if __name__ == '__main__':
-    sess = people.genSess()
+@retry(
+    stop_max_attempt_number=10  # 最大重试次数, 这玩意是很容易失败的
+)
+def __findNowBill(sess: Session, stuId: str, cardPwd: str) -> str:
+    log("正在登录...")
     imgCode = __getCode(sess)
-    peo = people.create()
     loginParams = {
-        "j_username": peo.name,
-        "j_password": peo.pwd,
+        "j_username": stuId,
+        "j_password": cardPwd,
         "imageCodeName": imgCode,
     }
-    sess.post(__login, params=loginParams)
+    loginRes = sess.post(__login, params=loginParams)
+    # debug("登陆:"+str(loginRes.content))
+
+    log("正在查询电费余额...")
+    billRes = sess.post(__bill, params=__elecItem)
+    log("电费余额(kWh):"+str(billRes.json()['restElecDegree']))
+    return str(billRes.json()['restElecDegree'])
+
+
+def __feeAdd(sess: Session, nowBill: str, addFee: str, payPwd: str):
+    log("本次充值金额: "+addFee)
+    prePayJson = {
+        'elcsysid': int(__elecItem['sysid']),
+        'elcarea': int(__elecItem['elcarea']),
+        'elcbuis': int(__elecItem['elcbuis']),
+        'roomNo': int(__elecItem['roomNo']),
+        'dumpEnergy': float(nowBill),
+        'paidMoney': float(addFee),
+        'route': None,
+    }
+    log("正在获取订单...")
+    prePayRes = sess.post(__prePay, params=prePayJson)
+    # debug("订单html:"+str(prePayRes.content))
+    soup = BeautifulSoup(prePayRes.content, "lxml")
+    billNo = soup.find(id='billno')['value']
+    refNo = soup.find(id='refno')['value']
+    log('充值中...订单编号:'+str(refNo))
+
+    realPayJson = {
+        'billno': billNo,
+        'refno': refNo,
+        'status': 0,
+        'banktype': None,
+        'paypwd': payPwd,
+    }
+    payRes = sess.post(__realPay, params=realPayJson)
+    debug(str(payRes.content))
+
+
+if __name__ == '__main__':
+    args = getStartArgs(4)
+    stuId, cardPwd, addFee, payPwd = args
+    sess = people.genSess()
+    nowBill = __findNowBill(sess, stuId, cardPwd)
+    addFee = "%.2f" % float(addFee)
+    __feeAdd(sess, nowBill, addFee, payPwd)
+
+#
+# nowBill = findNowBill(sess)
+
+# log("请输入充值金额(RMB):")
+# addFee = "%.2f" % float(input())
+# log(addFee)
 
 
 # 楼栋id列表 buiId就是楼id
--- util/utils.py
+++ util/utils.py
@@ -1,3 +1,7 @@
+import sys
+from typing import List
+
+
 def lower_json(json_info):
     if isinstance(json_info, dict):
         for key in list(json_info.keys()):
@@ -12,3 +16,10 @@ def lower_json(json_info):
     elif isinstance(json_info, list):
         for item in json_info:
             lower_json(item)
+
+
+def getStartArgs(count: int) -> list[str]:
+    res = []
+    for c in range(count):
+        res.append(sys.argv[c+1])
+    return res
